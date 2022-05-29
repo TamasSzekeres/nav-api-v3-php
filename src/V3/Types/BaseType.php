4cb62d324f39861fceb327fb9ed2a3775150bebc
Tamás Szekeres <szektam2@gmail.com>
2022-05-29 22:34:26 +0200

    Mod BaseType

--- src/V3/Types/BaseType.php
+++ src/V3/Types/BaseType.php
@@ -1,11 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LightSideSoftware\NavApi\V3\Types;
 
+use JMS\Serializer\Handler\HandlerRegistry;
 use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
 use JMS\Serializer\SerializerBuilder;
 use JMS\Serializer\Visitor\Factory\XmlSerializationVisitorFactory;
 use LightSideSoftware\NavApi\V3\Base\BaseObject;
+use LightSideSoftware\NavApi\V3\Serialization\DateTimeHandler;
+use LightSideSoftware\NavApi\V3\Serialization\EnumHandler;
 
 /**
  * BaseType
@@ -14,9 +19,13 @@ use LightSideSoftware\NavApi\V3\Base\BaseObject;
  */
 abstract class BaseType extends BaseObject
 {
-    public static function fromXml(string $xml): self
+    public static function fromXml(string $xml): static
     {
         return SerializerBuilder::create()
+            ->configureHandlers(function (HandlerRegistry $registry) {
+                $registry->registerSubscribingHandler(new DateTimeHandler());
+                $registry->registerSubscribingHandler(new EnumHandler());
+            })
             ->setPropertyNamingStrategy(new IdenticalPropertyNamingStrategy())
             ->build()
             ->deserialize($xml, static::class, 'xml');
@@ -27,6 +36,10 @@ abstract class BaseType extends BaseObject
         $serializationVisitorFactory = new XmlSerializationVisitorFactory();
 
         return SerializerBuilder::create()
+            ->configureHandlers(function (HandlerRegistry $registry) {
+                $registry->registerSubscribingHandler(new DateTimeHandler());
+                $registry->registerSubscribingHandler(new EnumHandler());
+            })
             ->setPropertyNamingStrategy(new IdenticalPropertyNamingStrategy())
             ->setSerializationVisitor('xml', $serializationVisitorFactory->setFormatOutput($indent))
             ->build()
