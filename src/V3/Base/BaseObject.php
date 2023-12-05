8e6dcfc73a5f27b83976527e63ebdb76155ffbef
Szekeres Tamás <szektam2@gmail.com>
2023-12-05 16:16:11 +0100

    feat: update BaseObject

--- src/V3/Base/BaseObject.php
+++ src/V3/Base/BaseObject.php
@@ -4,51 +4,19 @@ declare(strict_types=1);
 
 namespace LightSideSoftware\NavApi\V3\Base;
 
-use ArrayAccess;
 use ReflectionClass;
 use ReflectionProperty;
-use UnexpectedValueException;
 
-use function array_map;
 use function array_combine;
-use function property_exists;
+use function array_map;
 
 /**
- * Abstract base class for all object.
+ * Absztrakt ősosztály minden objektum számára.
  *
- * @author Tamás Szekeres <szektam2@gmail.com>
+ * @author Szekeres Tamás <szektam2@gmail.com>
  */
-abstract readonly class BaseObject implements ArrayAccess
+abstract readonly class BaseObject
 {
-    public function offsetExists($offset): bool
-    {
-        return property_exists($this, $offset);
-    }
-
-    public function offsetGet($offset): mixed
-    {
-        if (!$this->offsetExists($offset)) {
-            throw new UnexpectedValueException("Unknown offset: ${offset}");
-        }
-        return $this->$offset;
-    }
-
-    public function offsetSet($offset, $value): void
-    {
-        if (!$this->offsetExists($offset)) {
-            throw new UnexpectedValueException("Unknown offset: ${offset}");
-        }
-        $this->$offset = $value;
-    }
-
-    public function offsetUnset($offset): void
-    {
-        if (!$this->offsetExists($offset)) {
-            throw new UnexpectedValueException("Unknown offset: ${offset}");
-        }
-        $this->$offset = null;
-    }
-
     /**
      * @return array<string, mixed> [name => type]
      */
