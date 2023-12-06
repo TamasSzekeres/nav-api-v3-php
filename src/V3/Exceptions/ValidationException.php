ca9ae35083e908038524f342cfb969dbdcd63124
Szekeres Tamás <szektam2@gmail.com>
2023-12-06 17:24:32 +0100

    fix: ValidationException

--- src/V3/Exceptions/ValidationException.php
+++ src/V3/Exceptions/ValidationException.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LightSideSoftware\NavApi\V3\Exceptions;
 
 use Exception;
+use LightSideSoftware\NavApi\V3\Types\Validation\ErrorBag;
 use Throwable;
 
 /**
@@ -11,15 +14,15 @@ use Throwable;
 class ValidationException extends Exception
 {
     public function __construct(
-        string $message = '',
-        private readonly array $validationErrors = [],
+        string $message,
+        private readonly ErrorBag $validationErrors,
         int $code = 0,
         ?Throwable $previous = null
     ) {
         parent::__construct($message, $code, $previous);
     }
 
-    public function getValidationErrors(): array
+    public function getValidationErrors(): ErrorBag
     {
         return $this->validationErrors;
     }
